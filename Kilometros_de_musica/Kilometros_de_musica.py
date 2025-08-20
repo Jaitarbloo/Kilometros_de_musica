726e77b241c91c5bc43f404f937b1d3d97be2256
Jaitarbloo <jaitarbloo@yahoo.es>
2025-08-20 12:47:45 +0200

    Primer commit

---
+++ Kilometros_de_musica/Kilometros_de_musica.py
@@ -0,0 +1,31 @@
+
+import reflex as rx
+from Video_Inicial import video_inicial
+from Los_90 import Los_90
+from Navbar import navbar
+#from Prueba_componente import Prueba
+
+from rxconfig import config
+
+class State(rx.State):
+    
+    pass
+
+
+def index():
+    
+    return rx.vstack(
+
+                        navbar(),
+                        video_inicial(),
+                        Los_90(),
+                        #Prueba(),
+
+
+                    )
+
+    
+
+
+app = rx.App()
+app.add_page(index)
